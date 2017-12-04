1be0de7d65961070714f0973478cae67a0c3fe92
Adam Kunicki <adam@streamsets.com>
2017-12-04 13:14:44 -0800

    SDCE-160. Kafka Destination
    Change-Id: Ia062f932a2173a3ce1ea829a61f27c18939b07ac

--- stages/destinations/destinations.go
+++ stages/destinations/destinations.go
@@ -18,6 +18,7 @@ package destinations
 import (
 	_ "github.com/streamsets/datacollector-edge/stages/destinations/coap"
 	_ "github.com/streamsets/datacollector-edge/stages/destinations/http"
+	_ "github.com/streamsets/datacollector-edge/stages/destinations/kafka"
 	_ "github.com/streamsets/datacollector-edge/stages/destinations/mqtt"
 	_ "github.com/streamsets/datacollector-edge/stages/destinations/trash"
 	_ "github.com/streamsets/datacollector-edge/stages/destinations/websocket"
